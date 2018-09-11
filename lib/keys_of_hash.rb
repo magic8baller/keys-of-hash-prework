08450fad7602d419fb84299ebfd241f5e0a1c974
Leslie Turis <lturis@gmail.com>
2018-09-11 22:59:11 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,7 +3,7 @@
 class Hash
   def keys_of(*arguments)
     keys_array = []
-    arguments.each do |key, value|
+    self.map do |key, value|
       if arguments.include?(value)
         keys_array << key
       end
